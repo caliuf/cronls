7a6b84519788b8bf576b23b07bd1fa892010d8e2
CaioK <francesco.caliumi@gmail.com>
2016-05-27 18:28:06 +0200

    Add a couple of command line args and enhance dir and file check

--- cronls/args.py
+++ cronls/args.py
@@ -4,8 +4,7 @@ import os
 import argparse
 import datetime
 
-
-# -------------------------------------------------------------------- #
+# ==================================================================== #
 
 def check_datetime(value):
 	# Case "now"
@@ -44,7 +43,7 @@ def check_dir(value):
 
 	try:
 		os.listdir(value)
-	except OSError as e:
+	except IOError as e:
 		raise argparse.ArgumentTypeError('%s: %s' % (value,e.strerror))
 
 	return value
@@ -52,11 +51,27 @@ def check_dir(value):
 # -------------------------------------------------------------------- #
 
 def check_file(value):
-	pass
 
-# -------------------------------------------------------------------- #
+	if not os.path.exists(value):
+		raise argparse.ArgumentTypeError('%s: Invalid path' % value)
+	if not os.path.isfile(value):
+		raise argparse.ArgumentTypeError('%s: Not a file' % value)
+
+	try:
+		open(value, 'r')
+	except OSError as e:
+		raise argparse.ArgumentTypeError('%s: %s' % (value, e.strerror))
+
+	return value
+
+
+# ==================================================================== #
 
 def parse_cmd_args(argv):
+
+	# -------------------------------------- #
+	# Parser definition
+
 	parser = argparse.ArgumentParser(
 		description='',
 		epilog='',
@@ -76,6 +91,14 @@ def parse_cmd_args(argv):
 		nargs='?',
 		default='+24',
 		help='Stop date or datetime. Admitted formats: "now", "[+|-]hh" (e.g. "+24" or "-4") or "yy/mm/dd[-HH:MM]"'
+	)
+
+	parser.add_argument(
+		'-a', '--all',
+		dest="all",
+		action='store_true',
+		default=False,
+		help='Search for all users and system crons instead for the current user only'
 	)
 
 	parser.add_argument(
@@ -99,20 +122,45 @@ def parse_cmd_args(argv):
 		'-d', '--cron-dir',
 		dest="cron_dir",
 		action='store',
-		type=check_dir,
+		type=str,
 		default='/var/spool/cron',
 		help='Directory where cronls looks for cron files'
 	)
 
+	parser.add_argument(
+		'--sys-cron-file',
+		dest="sys_cron_file",
+		action='store',
+		type=None,
+		default='/etc/crontab',
+		help='File that cronls scans for system crons'
+	)
+	# -------------------------------------- #
+
+	# -------------------------------------- #
+	# Arguments list parsing
+
 	args = parser.parse_args(argv)
+	# -------------------------------------- #
+
+	# -------------------------------------- #
+	# Post parsing checks
 
 	if args.start_time >= args.stop_time:
 		argparse.ArgumentTypeError('Start time (%s) is higher then stop time (%s)' % (args.start_time, args.stop_time))
 
+	if args.all:
+		check_dir(args.cron_dir)
+
+		if args.system_cron:
+			check_file(args.system_cron)
+	# -------------------------------------- #
+
 	return args
 
-# -------------------------------------------------------------------- #
+# ==================================================================== #
 
 if __name__ == '__main__':
-	args = parse_cmd_args("+2 -s -r 10 -d /var".split())
+	args = parse_cmd_args("+2 -s -r 10 -d /var --sys-cron-file /etc/hosts --all".split())
+	args = parse_cmd_args("+2 -s -r 10".split())
 	print(args)
